bebd49da9a88ae795cedff830a4d95e1209d3dab
Martin Wickham <spexguy070@gmail.com>
2021-12-22 00:16:47 -0600

    day 22

--- src/day22.zig
+++ src/day22.zig
@@ -5,14 +5,141 @@ const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
 const Str = []const u8;
+const int = i64;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day22.txt");
 
+const Command = struct {
+    op: enum{ on, off },
+    bounds: Bounds,
+};
+
+const Bounds = struct {
+    v: [3][2]int,
+
+    fn size(self: @This()) usize {
+        return @intCast(usize,
+                (self.v[0][1] - self.v[0][0] + 1) *
+                (self.v[1][1] - self.v[1][0] + 1) *
+                (self.v[2][1] - self.v[2][0] + 1));
+    }
+
+    fn intersects(a: @This(), b: @This()) bool {
+        return (a.v[0][0] <= b.v[0][1] and a.v[0][1] >= b.v[0][0]) and
+                (a.v[1][0] <= b.v[1][1] and a.v[1][1] >= b.v[1][0]) and
+                (a.v[2][0] <= b.v[2][1] and a.v[2][1] >= b.v[2][0]);
+    }
+
+    fn contains(a: @This(), b: @This()) bool {
+        return (a.v[0][0] <= b.v[0][0] and a.v[0][1] >= b.v[0][1]) and
+                (a.v[1][0] <= b.v[1][0] and a.v[1][1] >= b.v[1][1]) and
+                (a.v[2][0] <= b.v[2][0] and a.v[2][1] >= b.v[2][1]);
+    }
+};
+
 pub fn main() !void {
-    
+    var timer = try std.time.Timer.start();
+
+    const items = util.parseLinesDelim(Command, data, " ,xyz=.");
+
+    const parse_time = timer.lap();
+
+    const part1_region = Bounds{ .v = [_][2]int{ .{-50, 50} } ** 3 };
+    var part1_regions = std.ArrayList(Bounds).init(gpa);
+    const part1_end_index = for (items) |it, i| {
+        if (!part1_region.contains(it.bounds)) break i;
+        try processCommand(it, &part1_regions);
+    } else unreachable;
+
+    var part1: usize = 0;
+    for (part1_regions.items) |r| {
+        part1 += r.size();
+    }
+
+    const part1_time = timer.lap();
+
+    var part2_regions = std.ArrayList(Bounds).init(gpa);
+    for (items[part1_end_index..]) |it| {
+        try processCommand(it, &part2_regions);
+    }
+
+    var part2: usize = part1;
+    for (part2_regions.items) |r| {
+        part2 += r.size();
+    }
+
+    const part2_time = timer.lap();
+
+    print("part1={}, part2={}\n", .{part1, part2});
+    print("Timing: parse={}, part1={}, part2={}, total={}\n", .{parse_time, part1_time, part2_time, parse_time + part1_time + part2_time});
+}
+
+fn processCommand(cmd: Command, regions: *std.ArrayList(Bounds)) !void {
+    switch (cmd.op) {
+        .on => {
+            var i: usize = 0;
+            next_region: while (i < regions.items.len) {
+                const item = regions.items[i];
+                if (item.intersects(cmd.bounds)) {
+                    if (item.contains(cmd.bounds)) {
+                        return; // all pixels already on
+                    }
+                    if (cmd.bounds.contains(item)) {
+                        _ = regions.swapRemove(i);
+                        continue :next_region;
+                    }
+                    var carved = CarvedList.init(0) catch unreachable;
+                    carve(&carved, item, cmd.bounds);
+                    try regions.replaceRange(i, 1, carved.slice());
+                    i += carved.len;
+                } else {
+                    i += 1;
+                }
+            }
+            try regions.append(cmd.bounds);
+        },
+        .off => {
+            var i: usize = 0;
+            next_region: while (i < regions.items.len) {
+                const item = regions.items[i];
+                if (item.intersects(cmd.bounds)) {
+                    if (cmd.bounds.contains(item)) {
+                        _ = regions.swapRemove(i);
+                        continue :next_region;
+                    }
+                    var carved = CarvedList.init(0) catch unreachable;
+                    carve(&carved, item, cmd.bounds);
+                    try regions.replaceRange(i, 1, carved.slice());
+                    i += carved.len;
+                } else {
+                    i += 1;
+                }
+            }
+        },
+    }
+}
+
+const CarvedList = std.BoundedArray(Bounds, 6);
+fn carve(result: *CarvedList, carved: Bounds, carver: Bounds) void {
+    var remain = carved;
+    var axis: usize = 0;
+    while (axis < 3) : (axis += 1) {
+        if (remain.v[axis][0] < carver.v[axis][0]) {
+            var chunk = remain;
+            chunk.v[axis][1] = carver.v[axis][0] - 1;
+            result.appendAssumeCapacity(chunk);
+            remain.v[axis][0] = carver.v[axis][0];
+        }
+        if (remain.v[axis][1] > carver.v[axis][1]) {
+            var chunk = remain;
+            chunk.v[axis][0] = carver.v[axis][1] + 1;
+            result.appendAssumeCapacity(chunk);
+            remain.v[axis][1] = carver.v[axis][1];
+        }
+    }
 }
 
 // Useful stdlib functions
@@ -27,6 +154,9 @@ const lastIndexOfStr = std.mem.lastIndexOfLinear;
 const trim = std.mem.trim;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
+const eql = std.mem.eql;
+
+const parseEnum = std.meta.stringToEnum;
 
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
--- src/util.zig
+++ src/util.zig
@@ -6,11 +6,194 @@ const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
 const Str = []const u8;
 
-var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
+pub var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = gpa_impl.allocator();
 
 // Add utility functions here
+pub const default_delims = " ,:(){}<>[]!\r\n\t";
 
+pub fn parseLines(comptime T: type, text: []const u8) []T {
+    return parseLinesDelim(T, text, default_delims);
+}
+
+pub fn parseLinesDelim(comptime T: type, text: []const u8, delims: []const u8) []T {
+    var list = List(T).init(gpa);
+    var lines = tokenize(u8, text, "\r\n");
+    var linenum: u32 = 1;
+    while (lines.next()) |line| : (linenum += 1) {
+        if (line.len == 0) { continue; }
+        list.append(parseLine(T, line, delims, linenum)) catch unreachable;
+    }
+    return list.toOwnedSlice();
+}
+
+pub fn parse(comptime T: type, str: []const u8) T {
+    return parseLine(T, str, default_delims, 1337);
+}
+
+pub fn parseLine(comptime T: type, str: []const u8, delims: []const u8, linenum: u32) T {
+    var it = std.mem.tokenize(u8, str, delims);
+    const result = parseNext(T, &it, linenum).?;
+    if (it.next()) |next| {
+        debugError("Extra token on line {}: '{s}'", .{linenum, next});
+    }
+    return result;
+}
+
+pub fn parseNext(comptime T: type, it: *std.mem.TokenIterator(u8), linenum: u32) ?T {
+    if (T == []const u8) return it.next();
+    switch (@typeInfo(T)) {
+        .Int => {
+            const token = it.next() orelse return null;
+            return parseInt(T, token, 10)
+                catch |err| debugError("invalid integer '{s}' on line {}, err={}", .{token, linenum, err});
+        },
+        .Float => {
+            const token = it.next() orelse return null;
+            return parseFloat(T, token)
+                catch |err| debugError("invalid float '{s}' on line {}, err={}", .{token, linenum, err});
+        },
+        .Enum => {
+            const token = it.next() orelse return null;
+            return strToEnum(T, token)
+                orelse debugError("cannot convert '{s}' to enum {s} on line {}", .{token, @typeName(T), linenum});
+        },
+        .Array => |arr| {
+            var result: T = undefined;
+            for (result) |*item, i| {
+                item.* = parseNext(arr.child, it, linenum) orelse {
+                    if (i == 0) { return null; }
+                    debugError("Only found {} of {} items in array, on line {}\n", .{i, arr.len, linenum});
+                };
+            }
+            return result;
+        },
+        .Struct => |str| {
+            var result: T = undefined;
+            _ = str;
+            var exit: bool = false; // workaround for control flow in inline for issues
+            inline for (str.fields) |field, i| {
+                parseNextStructField(&result, field, i, &exit, it, linenum);
+            }
+            if (exit) return null;
+            return result;
+        },
+        .Optional => |opt| {
+            return @as(T, parseNext(opt.child, it, linenum));
+        },
+        .Pointer => |ptr| {
+            if (ptr.size == .Slice) {
+                var results = List(ptr.child).init(gpa);
+                while (parseNext(ptr.child, it, linenum)) |value| {
+                    results.append(value) catch unreachable;
+                }
+                return results.toOwnedSlice();
+            } else @compileError("Unsupported type "++@typeName(T));
+        },
+        else => @compileError("Unsupported type "++@typeName(T)),
+    }
+}
+
+fn parseNextStructField(
+    result: anytype,
+    comptime field: std.builtin.TypeInfo.StructField,
+    comptime i: usize,
+    exit: *bool,
+    it: *std.mem.TokenIterator(u8),
+    linenum: u32,
+) void {
+    if (!exit.*) {
+        if (field.name[0] == '_') {
+            @field(result, field.name) = field.default_value orelse undefined;
+        } else if (parseNext(field.field_type, it, linenum)) |value| {
+            @field(result, field.name) = value;
+        } else if (field.default_value) |default| {
+            @field(result, field.name) = default;
+        } else if (i == 0) {
+            exit.* = true;
+        } else if (comptime std.meta.trait.isSlice(field.field_type)) {
+            @field(result, field.name) = &.{};
+        } else {
+            debugError("Missing field {s}.{s} and no default, on line {}", .{@typeName(@TypeOf(result)), field.name, linenum});
+        }
+    }
+}
+
+test "parseLine" {
+    assert(parseLine(u32, " 42 ", " ,", @src().line) == 42);
+    assert(parseLine(f32, " 0.5", " ,", @src().line) == 0.5);
+    assert(parseLine(f32, "42", " ,", @src().line) == 42);
+    assert(parseLine(enum { foo, bar }, "foo", " ,", @src().line) == .foo);
+    assert(eql(u16, &parseLine([3]u16, " 2, 15 4 ", " ,", @src().line), &[_]u16{2, 15, 4}));
+    assert(eql(u16, parseLine([]u16, " 2, 15 4 ", " ,", @src().line), &[_]u16{2, 15, 4}));
+    assert(parseLine(?f32, "42", " ,", @src().line).? == 42);
+    assert(parseLine(?f32, "", " ,", @src().line) == null);
+    assert(eql(u8, parseLine([]const u8, "foob", " ,", @src().line), "foob"));
+    assert(eql(u8, parseLine([]const u8, "foob", " ,", @src().line), "foob"));
+    assert(eql(u8, parseLine(Str, "foob", " ,", @src().line), "foob"));
+
+    const T = struct {
+        int: i32,
+        float: f32,
+        enumeration: enum{ foo, bar, baz },
+        _first: bool = true,
+        array: [3]u16,
+        string: []const u8,
+        _skip: *@This(),
+        optional: ?u16,
+        tail: [][]const u8,
+    };
+
+    {
+        const a = parseLine(T, "4: 5.0, bar 4, 5, 6 badaboom", ":, ", @src().line);
+        assert(a.int == 4);
+        assert(a.float == 5.0);
+        assert(a.enumeration == .bar);
+        assert(a._first == true);
+        assert(eql(u16, &a.array, &[_]u16{4, 5, 6}));
+        assert(eql(u8, a.string, "badaboom"));
+        assert(a.optional == null);
+        assert(a.tail.len == 0);
+    }
+
+    {
+        const a = parseLine(T, "-5: 3: foo 4, 5, 6 booptroop 53", ":, ", @src().line);
+        assert(a.int == -5);
+        assert(a.float == 3);
+        assert(a.enumeration == .foo);
+        assert(a._first == true);
+        assert(eql(u16, &a.array, &[_]u16{4, 5, 6}));
+        assert(eql(u8, a.string, "booptroop"));
+        assert(a.optional.? == 53);
+        assert(a.tail.len == 0);
+    }
+
+    {
+        const a = parseLine(T, "+15: -10: baz 5, 6, 7 skidoosh 82 ruby supports bare words", ":, ", @src().line);
+        assert(a.int == 15);
+        assert(a.float == -10);
+        assert(a.enumeration == .baz);
+        assert(a._first == true);
+        assert(eql(u16, &a.array, &[_]u16{5, 6, 7}));
+        assert(eql(u8, a.string, "skidoosh"));
+        assert(a.optional.? == 82);
+        assert(a.tail.len == 4);
+        assert(eql(u8, a.tail[0], "ruby"));
+        assert(eql(u8, a.tail[1], "supports"));
+        assert(eql(u8, a.tail[2], "bare"));
+        assert(eql(u8, a.tail[3], "words"));
+    }
+
+    print("All tests passed.\n", .{});
+}
+
+inline fn debugError(comptime fmt: []const u8, args: anytype) noreturn {
+    if (std.debug.runtime_safety) {
+        std.debug.panic(fmt, args);
+    } else {
+        unreachable;
+    }
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
@@ -24,6 +207,9 @@ const lastIndexOfStr = std.mem.lastIndexOfLinear;
 const trim = std.mem.trim;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
+const eql = std.mem.eql;
+
+const strToEnum = std.meta.stringToEnum;
 
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
